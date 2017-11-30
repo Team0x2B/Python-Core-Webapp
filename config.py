e904e7092671b88af630267ff4195d4565eb8524
Ethan Hunter <fc.spaceman@gmail.com>
2017-11-29 22:36:18 -0700

    add better default secret key

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 database_uri = os.environ.get("DATABASE_URL", "sqlite:///studi_data.db")
 
-secret_key = os.environ.get("SECRET_KEY", None)
+secret_key = os.environ.get("SECRET_KEY", "maybe-dont-use-this")
 
