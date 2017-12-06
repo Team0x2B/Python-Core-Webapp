2585751777400a73f9897b5256fa96ea4e948fa7
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-05 23:33:12 -0700

    fix secret-key

--- studi/__init__.py
+++ studi/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] = config.secret_key
 db = SQLAlchemy(app)
 
 from studi import user, studygroup  # import all ORM objects here
--- studi/config.py
+++ studi/config.py
@@ -2,3 +2,8 @@ import os
 
 
 database_uri = os.environ.get("DATABASE_URL", "sqlite:///studi_data.db")
+
+secret_key = os.environ.get("SECRET_KEY", "please-change-this")
+
+if secret_key == "please-change-this":
+    print("secret-key not set. Hopefully you're in debug.")
