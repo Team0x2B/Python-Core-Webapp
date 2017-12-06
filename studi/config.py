60e52673929782ddb96b8373417d24c438fc7df7
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-05 23:44:50 -0700

    probably decode secretkey

--- studi/config.py
+++ studi/config.py
@@ -1,9 +1,11 @@
 import os
+from base64 import b64decode
 
 
 database_uri = os.environ.get("DATABASE_URL", "sqlite:///studi_data.db")
 
-secret_key = os.environ.get("SECRET_KEY", "please-change-this")
-
-if secret_key == "please-change-this":
+if "SECRET_KEY" in os.environ:
+    secret_key = b64decode(os.environ["SECRET_KEY"], 'utf-8')
+else:
+    secret_key = "please-change-this"
     print("secret-key not set. Hopefully you're in debug.")
