6937f44a2ef4b7fcc1fb42ec0a346e8a51d51422
Trevor Jedziniak <32111962+trje9656@users.noreply.github.com>
2017-12-06 11:02:23 -0700

    removed requirement for server.pem

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ import os
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000,
-    ssl_context=('./server.pem','./server.pem'))
+    app.run(debug=True,host='0.0.0.0', port=4000)
