8bec0df710fad0b63ebe8dd5d46f71e61d0a9b8d
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-05 23:18:47 -0700

    add message when secret key not set

--- studi/config.py
+++ studi/config.py
@@ -4,3 +4,6 @@ import os
 database_uri = os.environ.get("DATABASE_URL", "sqlite:///studi_data.db")
 
 secret_key = os.environ.get("SECRET_KEY", "maybe-dont-use-this")
+
+if secret_key == "maybe-dont-use-this":
+    print("SECRET_KEY not properly set -- hopefully this is debug")
