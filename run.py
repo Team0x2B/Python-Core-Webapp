5f87adc88acc4a77cc2cdfb75343e4d9b84c332e
Trevor Jedziniak <32111962+trje9656@users.noreply.github.com>
2017-12-05 21:47:03 -0700

    added server.pem to force use https

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ import os
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(debug=False, host='0.0.0.0', port=4000)
+    app.run(debug=True,host='0.0.0.0', port=4000,
+    ssl_context=('./server.pem','./server.pem'))
