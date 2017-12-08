89e90d0c2d796aff6265dfa73f96db766f3ed439
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-08 14:25:01 -0700

    fix info window and make pins smaller

--- studi/static/js/main/mabbox.js
+++ studi/static/js/main/mabbox.js
@@ -1,44 +1,55 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3BhY2VtYW4xNzAxIiwiYSI6ImNqYXk5aDJ4ZjE1bmMyd21rNjh0cnY4NnEifQ.oGvBAyBD4Az0Ij9o2ThX9A';
 map_pin_url = '../static/css/images/pin.svg';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+
+
+var map;
+navigator.geolocation.getCurrentPosition(function(position){
+    console.log("centering map");
+    map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        pitchWithRotate: false,
+        zoom: 15,
+        center: [position.coords.longitude, position.coords.latitude]
+    });
+
+    map.on('load', onMapLoad);
+    map.on('click', onMapClick);
+
+}, function error (msg) {
+    console.log("geolocation error");
 });
 
-map.on('load', function(event) {
+function onMapLoad(event) {
     console.log("map loaded");
-    navigator.geolocation.getCurrentPosition(function(position){
-        console.log("centering map");
-        lngLat = [position.coords.longitude, position.coords.latitude]
-        map.jumpTo({center: lngLat, zoom: 16})
-    }, function error (msg) {
-        console.log("geolocation error");
-    });
+    map.setLayerZoomRange('building', 0, 20);
     $.getJSON( "api/get_study_groups", function( data ){
             $.each( data, function( key, val ){
                 var el = document.createElement('div');
                 el.className = 'marker';
-                el.style.backgroundImage = 'url(' + map_pin_url + ')';
-                el.style.width = '50' + 'px';
-                el.style.height = '50' + 'px';
+                var img = document.createElement('img');
+                img.setAttribute("src", map_pin_url);
+                img.style.width = '35px';
+                img.style.height = '35px';
+                el.appendChild(img);
 
                 new mapboxgl.Marker(el)
                     .setLngLat([val.longitude, val.latitude])
                     .addTo(map);
 
-                el.addEventListener('click', function() {
+                el.addEventListener('click', function(event) {
+                    event.stopImmediatePropagation()
                     onMarkerClick(key, data[key]);
                 });
             });
     });
-});
+}
 
-map.on('click', function(event) {
+function onMapClick(event) {
     console.log("clicked");
     console.log(event);
-
-
-});
+    hideInfoWindow();
+}
 
 function onMarkerClick(key, group_data) {
     console.log(group_data);
