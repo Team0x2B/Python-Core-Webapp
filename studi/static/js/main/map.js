80f1700238157a4f400357306dff71e1d016ee8c
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-07 18:12:46 -0700

    fix js bug when there's a geolocation error

--- studi/static/js/main/map.js
+++ studi/static/js/main/map.js
@@ -46,7 +46,7 @@ function initMap() {
             });
         });
     }, function error (msg) {
-        alter("geolocation error!")
+        alert("geolocation error!")
     }, {maximumAge:600000, timeout:5000, enableHighAccuracy: true}); // end getCurrentPosition
     console.log("hello");
 };
