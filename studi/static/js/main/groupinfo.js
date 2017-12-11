b53147acdafb336f9f90c311454a409986be89c2
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-11 14:00:09 -0700

    improve groupinfo refresh performance

--- studi/static/js/main/groupinfo.js
+++ studi/static/js/main/groupinfo.js
@@ -1,5 +1,4 @@
-var known_group_members = []
-
+var current_group_id = -1;
 function get_group_info(group_id) {
    return $.ajax({
         url: "/api/get_group_by_id/" + group_id,
@@ -37,11 +36,14 @@ function construct_group_info_popout(group_id) {
 
 function do_build_info_popout(group) {
     user_permissions = group.allowed_actions;
-    setText(document.getElementById("group-info-title"), group.topic);
-    setText(document.getElementById("group-info-subtitle"), group.dept + ": " + group.course_num);
-    setText(document.getElementById("group-info-author"), group.members[0].username);
-    setText(document.getElementById("group-info-time"), "Feature Coming Soon");
-    setText(document.getElementById("group-info-description"), group.desc);
+    if (current_group_id != group.id) {
+        setText(document.getElementById("group-info-title"), group.topic);
+        setText(document.getElementById("group-info-subtitle"), group.dept + ": " + group.course_num);
+        setText(document.getElementById("group-info-author"), group.members[0].username);
+        setText(document.getElementById("group-info-time"), "Feature Coming Soon");
+        setText(document.getElementById("group-info-description"), group.desc);
+        current_group_id = group.id;
+    }
 
     create_action_button(group.id, user_permissions);
     create_members_list(group.members);
@@ -66,37 +68,35 @@ function create_members_list(members) {
 
 function create_action_button(group_id, user_permissions) {
     detailed_info = document.getElementById("group-info-detailed-group-info")
+    var action_button;
     for (var i = 0; i < detailed_info.childNodes.length; i++) {
         if (detailed_info.childNodes[i].className == "action-button") {
-            detailed_info.removeChild(detailed_info.childNodes[i]);
-            break;
+            action_button = detailed_info.childNodes[i];
         }
     }
+    if (!action_button) {
+        action_button = document.createElement("a")
+        detailed_info.appendChild(action_button);
+    }
+    action_button.setAttribute("class", "action-button");
+    action_button.setAttribute("href", "javascript:void(0)");
     console.log(user_permissions);
     if (user_permissions.can_join) {
-        var join_button = document.createElement("a");
-        join_button.setAttribute('id', 'join-button');
-        join_button.setAttribute("class", "action-button");
-        join_button.setAttribute("href", "javascript:void(0)");
-        join_button.setAttribute("onclick", "joinGroup(" + group_id + ");");
-        setText(join_button, "Join Group");
-        detailed_info.appendChild(join_button);
+        action_button.setAttribute('id', 'join-button');
+        action_button.setAttribute("onclick", "joinGroup(" + group_id + ");");
+        setText(action_button, "Join Group");
     } else if (user_permissions.can_leave) {
-        var leave_button = document.createElement("a");
-        leave_button.setAttribute('id', 'leave-button');
-        leave_button.setAttribute("class", "action-button");
-        leave_button.setAttribute("href", "javascript:void(0)");
-        leave_button.setAttribute("onclick", "leaveGroup(" + group_id + ");");
-        setText(leave_button, "Leave Group");
-        detailed_info.appendChild(leave_button);
+        action_button.setAttribute('id', 'leave-button');
+        action_button.setAttribute("onclick", "leaveGroup(" + group_id + ");");
+        setText(action_button, "Leave Group");
     } else if (user_permissions.can_delete) {
-        var delete_button = document.createElement("a");
-        delete_button.setAttribute('id', 'delete-button');
-        delete_button.setAttribute("class", "action-button");
-        delete_button.setAttribute("href", "javascript:void(0)");
-        delete_button.setAttribute("onclick", "deleteGroup(" + group_id + ");");
-        setText(delete_button, "Delete Group");
-        detailed_info.appendChild(delete_button);
+        action_button.setAttribute('id', 'delete-button');
+        action_button.setAttribute("onclick", "deleteGroup(" + group_id + ");");
+        setText(action_button, "Delete Group");
+    } else {
+        if (action_button) {
+            detailed_info.removeChild(action_button);
+        }
     }
 }
 
